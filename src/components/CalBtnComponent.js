9f60b9d54d491cf1f5cbdacde8f86e483a290e3f
Deepak39 <deepakyadav53882@gmail.com>
2020-07-29 23:19:26 +0530

    btn-rows done.

--- src/components/CalBtnComponent.js
+++ src/components/CalBtnComponent.js
@@ -3,8 +3,9 @@ import React from 'react';
 const CalcButton = ({isDarkTheme, text}) => {
 
   const neumorph = isDarkTheme ? 'neumorph-btn-dark' : 'neumorph-btn-light';
+  const btnColor = isDarkTheme ? "dark-btn-color" : "light-btn-color";
 
-  return <span className={`cal-btn ${neumorph}`}> {text} </span>;
+  return <span className={`cal-btn ${btnColor} ${neumorph}`}> {text} </span>;
 }
 
 export default CalcButton;
--- src/components/CalculatorBodyComponent.js
+++ src/components/CalculatorBodyComponent.js
@@ -3,18 +3,34 @@ import CalcButton from './CalBtnComponent';
 
 const CalculatorBody = ({isDarkTheme}) => {
   
-  // const btns = [['7', '8', '9', '/'], ['4', '5', '6', '*'], ['1', '2', '3', '-'], ['0', '.', '%', '+']];
+  const btns = [['7', '8', '9', '/'], ['4', '5', '6', '*'], ['1', '2', '3', '-'], ['0', '.', '%', '+']];
   // const btnCol = ['C', 'S', 'S', '='];
 
   return (
     <div className="btn-container">
       <div className="btn-rows">
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
-        <div className="row"></div>
+        {/* <div className="row">
+          <CalcButton isDarkTheme={isDarkTheme} text={1} />
+          <CalcButton isDarkTheme={isDarkTheme} text={2} />
+          <CalcButton isDarkTheme={isDarkTheme} text={3} />
+          <CalcButton isDarkTheme={isDarkTheme} text={4} />
+        </div>
+        <div className="row">
+          <CalcButton isDarkTheme={isDarkTheme} text={2} />
+          <CalcButton isDarkTheme={isDarkTheme} text={1} />
+          <CalcButton isDarkTheme={isDarkTheme} text={1} />
+          <CalcButton isDarkTheme={isDarkTheme} text={1} />
+        </div> */}
+
+        {
+          btns.map((arr) => (
+            <div className="row">
+              {arr.map((val) => (<CalcButton isDarkTheme={isDarkTheme} text={val} />))}
+            </div>)
+          )
+        }
       </div>
-      <div>
+      <div className="btn-col">
         <div></div>
       </div>
     </div>
